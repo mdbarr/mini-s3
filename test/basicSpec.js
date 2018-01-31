c302762178630f32d8052cf3492d60f33766aa4a
Mark Barr <mbarr@onshape.com>
2018-01-30 23:07:23 -0500

    Start of data modeling.

--- test/basicSpec.js
+++ test/basicSpec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const MiniS3 = require('../lib/mini-s3');
 
 AWS.config.update({
   accessKeyId: 'accessKeyId',
@@ -12,11 +13,21 @@ const s3 = new AWS.S3({
   endpoint: 'http://localhost:9090'
 });
 
+const minis3 = new MiniS3();
+
 describe('Basic S3 Spec', function() {
+  before(function() {
+    return minis3.boot();
+  });
+
   it('should connect and list buckets', function() {
     return s3.listBuckets().promise().
       then(function(data) {
         console.log('Bucket List', data.Buckets);
       });
+  });
+
+  after(function() {
+    return minis3.shutdown();
   });
 });
